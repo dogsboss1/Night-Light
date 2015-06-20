ce4e8204ae68302ccd906abe6151a5ca8c289811
Felix King <felixking@Felixs-MacBook-Pro.local>
2015-06-20 13:52:45 +0800

    Lets get this thing started

---
+++ Night Light/PageContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageContentViewController.h
+//  Night Light
+//
+//  Created by felix king on 20/06/2015.
+//  Copyright (c) 2015 Felix King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageContentViewController : UIViewController
+
+@end
---
+++ Night Light/PageContentViewController.m
@@ -0,0 +1,13 @@
+//
+//  PageContentViewController.m
+//  Night Light
+//
+//  Created by felix king on 20/06/2015.
+//  Copyright (c) 2015 Felix King. All rights reserved.
+//
+
+#import "PageContentViewController.h"
+
+@implementation PageContentViewController
+
+@end
