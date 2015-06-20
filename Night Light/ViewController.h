5e5a6bf168d20e4289bf32197c035801d487e025
Felix King <felixking@Felixs-MacBook-Pro.local>
2015-06-20 13:29:53 +0800

    Initial Commit

---
+++ Night Light/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Night Light
+//
+//  Created by felix king on 20/06/2015.
+//  Copyright (c) 2015 Felix King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
